fcc8043816b71a72c2caadd66346547039374a2f
fish <22388145+chaoyenpo@users.noreply.github.com>
2022-02-08 17:03:31 +0800

    新增 Admin 的 Navbar

---
+++ resources/js/Components/Navbar.js
@@ -0,0 +1,83 @@
+import React from "react";
+import { Menu, Popover, Transition } from "@headlessui/react";
+import { SearchIcon } from "@heroicons/react/solid";
+import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
+import ApplicationLogo from "@/Components/ApplicationLogo";
+import Dropdown from "@/Components/Dropdown";
+
+export default function Navbar({ auth, userNavigation }) {
+    return (
+        <div className="mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="relative flex justify-between xl:grid xl:grid-cols-12 lg:gap-8">
+                <div className="flex lg:static xl:col-span-2">
+                    <div className="flex-shrink-0 flex items-center">
+                        <ApplicationLogo className="block h-9 w-auto text-gray-500" />
+                        <span className="ml-3">Zacian Admin</span>
+                    </div>
+                </div>
+                <div className="min-w-0 flex-1 md:px-8 lg:px-0 xl:col-span-6">
+                    <div className="flex items-center px-6 py-4 md:max-w-3xl md:mx-auto lg:max-w-none lg:mx-0 xl:px-0">
+                        <div className="w-full">
+                            <label htmlFor="search" className="sr-only">
+                                Search
+                            </label>
+                            <div className="relative">
+                                <div className="pointer-events-none absolute inset-y-0 left-0 pl-3 flex items-center">
+                                    <SearchIcon
+                                        className="h-5 w-5 text-gray-400"
+                                        aria-hidden="true"
+                                    />
+                                </div>
+                                <input
+                                    id="search"
+                                    name="search"
+                                    className="block w-full bg-white border border-gray-300 rounded-md py-2 pl-10 pr-3 text-sm placeholder-gray-500 focus:outline-none focus:text-gray-900 focus:placeholder-gray-400 focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                    placeholder="Search"
+                                    type="search"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex items-center justify-end xl:col-span-4">
+                    <a
+                        href="#"
+                        className="ml-5 flex-shrink-0 bg-white rounded-full p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                        <span className="sr-only">View notifications</span>
+                        <BellIcon className="h-6 w-6" aria-hidden="true" />
+                    </a>
+
+                    <div className="flex-shrink-0 relative ml-3">
+                        <Dropdown>
+                            <Dropdown.Trigger>
+                                <button
+                                    type="button"
+                                    className="bg-white rounded-full flex focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                >
+                                    <img
+                                        className="h-8 w-8 rounded-full"
+                                        src={`https://ui-avatars.com/api/?name=${auth.user.name}&color=7F9CF5&background=EBF4FF`}
+                                    />
+                                </button>
+                            </Dropdown.Trigger>
+
+                            <Dropdown.Content>
+                                {userNavigation.map((item) => (
+                                    <Dropdown.Link
+                                        key={item.name}
+                                        href={item.href}
+                                        method={item.method}
+                                        as={item.as}
+                                    >
+                                        {item.name}
+                                    </Dropdown.Link>
+                                ))}
+                            </Dropdown.Content>
+                        </Dropdown>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
