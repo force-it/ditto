77418848169d51f780228267ca1b7411e8f51803
fish <22388145+chaoyenpo@users.noreply.github.com>
2022-02-15 17:22:36 +0800

    Update

--- resources/js/Pages/Welcome.js
+++ resources/js/Pages/Welcome.js
@@ -5,14 +5,14 @@ export default function Welcome(props) {
     return (
         <>
             <Head title="Welcome" />
-            <div className="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+            <div className="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
                 <div className="fixed top-0 right-0 px-6 py-4 sm:block">
                     {props.auth.user ? (
                         <Link
                             href={route("dashboard")}
                             className="text-sm text-gray-700 underline"
                         >
-                            Dashboard
+                            進入 Zacian
                         </Link>
                     ) : (
                         <>
@@ -20,20 +20,20 @@ export default function Welcome(props) {
                                 href={route("login")}
                                 className="text-sm text-gray-700 underline"
                             >
-                                Log in
+                                登入
                             </Link>
 
-                            <Link
+                            {/* <Link
                                 href={route("register")}
                                 className="ml-4 text-sm text-gray-700 underline"
                             >
                                 Register
-                            </Link>
+                            </Link> */}
                         </>
                     )}
                 </div>
 
-                <div className="max-w-6xl mx-auto sm:px-6 lg:px-8">
+                <div className="hidden max-w-6xl mx-auto sm:px-6 lg:px-8">
                     {props.auth.user ? (
                         <Link
                             href={route("dashboard")}
--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,7 @@ use App\Http\Controllers\Admin\OrganizationController;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return redirect()->route('dashboard');
 });
 
 Route::get('/dashboard', function () {
