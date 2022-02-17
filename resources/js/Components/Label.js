a08a37d56791198ff1c8c8579d3109950175cd35
fish <22388145+chaoyenpo@users.noreply.github.com>
2022-02-17 17:56:54 +0800

    Update

--- resources/js/Components/Label.js
+++ resources/js/Components/Label.js
@@ -6,7 +6,7 @@ export default function Label({ forInput, value, className, children }) {
             htmlFor={forInput}
             className={`block font-medium text-sm text-gray-700 ` + className}
         >
-            {value ? value : { children }}
+            {value ? value : children}
         </label>
     );
 }
