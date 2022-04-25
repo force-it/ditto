af018b11039df10a6d894cccf8b9aaedbe21bcd5
fish <22388145+chaoyenpo@users.noreply.github.com>
2022-04-25 10:53:48 +0800

    Fix

--- app/Http/Controllers/AnalyticsController.php
+++ app/Http/Controllers/AnalyticsController.php
@@ -14,24 +14,24 @@ class AnalyticsController extends Controller
 {
    public function index(Request $request)
     {
-        $userLogins = UserLogin::selectRaw('
-                UNIX_TIMESTAMP(date_format(login_time, "%Y-%m-%d %H:%i")) as login_minute,
-                gl_user_login.*')
-            ->whereRaw('login_time > "2021-12-03 14:25:00"')
-            ->orderBy('login_minute', 'desc')
-            ->get()
-            ->map(function ($item) {
-                $item['login_minute'] = str_replace('.000000', '', $item['login_minute']);
-                return $item->only(['login_minute', 'user_id', 'coordinate',
-                    'device_category', 'app_category', 'client_category']);
-            });
+        // $userLogins = UserLogin::selectRaw('
+        //         UNIX_TIMESTAMP(date_format(login_time, "%Y-%m-%d %H:%i")) as login_minute,
+        //         gl_user_login.*')
+        //     ->whereRaw('login_time > "2021-12-03 14:25:00"')
+        //     ->orderBy('login_minute', 'desc')
+        //     ->get()
+        //     ->map(function ($item) {
+        //         $item['login_minute'] = str_replace('.000000', '', $item['login_minute']);
+        //         return $item->only(['login_minute', 'user_id', 'coordinate',
+        //             'device_category', 'app_category', 'client_category']);
+        //     });
 
-        $userLogins->groupBy('login_minute')->all();
+        // $userLogins->groupBy('login_minute')->all();
 
         // 裝置陣列
 
         return Inertia::render('Realtime/Overview', [
-            'userLogins' => $userLogins
+            'userLogins' => []
         ]);
     }
 }
